335dcb1cee01aa2f0aeac4e41fd8f3045cefc6c8
Marc Mansour <marcm9387@gmail.com>
2024-01-27 22:39:42 +0100

    add command menu on cv page

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -12,7 +12,7 @@ import {
   CommandSeparator,
 } from "@/components/ui/command";
 import { Button } from "./ui/button";
-import { CommandIcon } from "lucide-react";
+import MenuIcon from "./icons/MenuIcon";
 
 interface Props {
   links: { url: string; title: string }[];
@@ -35,20 +35,26 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 right-0 hidden border-t-muted p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
-        Press{" "}
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>J
-        </kbd>{" "}
-        to open the command menu
-      </p>
+      <div className="fixed bottom-0 right-0 hidden border-t-muted p-1 text-sm text-muted-foreground print:hidden xl:block text-pretty flex-col text-right">
+        <p>
+          Press{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">⌘</span>J
+          </kbd>{" "}
+          or{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">ctrl</span>J
+          </kbd>
+        </p>
+        <p>to open the command menu</p>
+      </div>
       <Button
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
         className="fixed bottom-20 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
       >
-        <CommandIcon className="my-6 size-6" />
+        <MenuIcon />
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder="Type a command or search..." />
